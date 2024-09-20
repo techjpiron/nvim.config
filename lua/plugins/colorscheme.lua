4cad935398dffd17682f5022b39d4afb3fc11d25
Julien Piron <tech@jpiron.eu>
2024-09-20 09:23:50 +0200

    chore: start from scratch

--- lua/plugins/colorscheme.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "shaunsingh/nord.nvim",
-  priority = 1000, -- make sure to load this before all the other start plugins
-  init = function()
-    vim.cmd.colorscheme("nord")
-  end,
-}
--- lua/plugins/terminal.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "akinsho/toggleterm.nvim",
-  version = "*",
-  config = function()
-    require("toggleterm").setup({})
-    vim.keymap.set("n", "<leader>t", "<cmd>:ToggleTerm direction=vertical size=50<cr>", { desc = "Open [T]erminal" })
-  end,
-}
