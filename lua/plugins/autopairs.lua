5fa76d4e40efe9e7082eea3a4b0359e3f0969c28
Julien Piron <tech@jpiron.eu>
2024-09-20 10:20:59 +0200

    add autopairs

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true,
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    config = true,
+  },
+}
