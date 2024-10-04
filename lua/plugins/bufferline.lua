22e0689ca99a873f77a9187f77fcb3a554864ac5
Julien Piron <tech@jpiron.eu>
2024-10-04 11:43:29 +0200

    add bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -4,7 +4,9 @@ return {
   dependencies = "nvim-tree/nvim-web-devicons",
   config = function()
     require("bufferline").setup({})
+
     vim.keymap.set("n", "L", "<cmd>BufferLineCycleNext<cr>")
     vim.keymap.set("n", "H", "<cmd>BufferLineCyclePrev<cr>")
+    vim.keymap.set("n", "<leader>c", "<cmd>bdelete<cr>")
   end,
 }
