f7ec31fafefce85a01095b1bba8b6c9a89e939c1
Julien Piron <tech@jpiron.eu>
2024-11-15 14:11:46 +0100

    change theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,9 +1,9 @@
 return {
   "folke/tokyonight.nvim",
-  lazy = false,
-  name = "tokyonight",
-  priority = 1000,
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    vim.cmd([[colorscheme tokyonight]])
+    -- load the colorscheme here
+    vim.cmd([[colorscheme tokyonight-moon]])
   end,
 }
