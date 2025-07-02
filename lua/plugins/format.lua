1b8eb7fb57feed99fa8a366e41fd1b19bd0acc4c
Julien Piron <tech@jpiron.eu>
2025-07-02 16:56:49 +0200

    add formatting for typescript and react

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -26,17 +26,16 @@ return {
         lsp_format_opt = "fallback"
       end
       return {
-        timeout_ms = 500,
+        timeout_ms = 1000,
         lsp_format = lsp_format_opt,
       }
     end,
     formatters_by_ft = {
       lua = { "stylua" },
-      -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
-      --
-      -- You can use 'stop_after_first' to run the first available formatter from the list
-      javascript = { "prettierd", "prettier", stop_after_first = true },
+      javascript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescript = { "prettier" },
+      typescriptreact = { "prettier" },
     },
   },
 }
